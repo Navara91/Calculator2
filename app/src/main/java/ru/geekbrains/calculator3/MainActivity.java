73d3bbf8b4e8e2bbf23384f75a04d5875a52ab7d
Nikolay <kuoluska@mail.ru>
2021-06-06 20:08:08 +0900

    create operators "if" for clickListener

--- app/src/main/java/ru/geekbrains/calculator3/MainActivity.java
+++ app/src/main/java/ru/geekbrains/calculator3/MainActivity.java
@@ -5,12 +5,73 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private View.OnClickListener clickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+
+            if(v.getId() == R.id.key_1){
+
+            }
+
+            if(v.getId() == R.id.key_2){
+
+            }
+
+            if(v.getId() == R.id.key_3){
+
+            }
+
+            if(v.getId() == R.id.key_4){
+
+            }
+
+            if(v.getId() == R.id.key_5){
+
+            }
+
+            if(v.getId() == R.id.key_6){
+
+            }
+
+            if(v.getId() == R.id.key_9){
+
+            }
+
+            if(v.getId() == R.id.key_0){
+
+            }
+
+            if(v.getId() == R.id.key_point){
+
+            }
+
+            if(v.getId() == R.id.key_plus){
+
+            }
+
+            if(v.getId() == R.id.key_minus){
+
+            }
+
+            if(v.getId() == R.id.key_multi){
+
+            }
+
+            if(v.getId() == R.id.key_division){
+
+            }
+
+            if(v.getId() == R.id.key_clear){
+
+            }
+
+            if(v.getId() == R.id.key_equal){
+
+            }
 
         }
     };
