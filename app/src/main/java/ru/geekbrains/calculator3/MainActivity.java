629121887697708d917708f65764a0da3d85d18c
Nikolay <kuoluska@mail.ru>
2021-06-06 21:31:37 +0900

    transport calculator logic in new class "operations"

--- app/src/main/java/ru/geekbrains/calculator3/MainActivity.java
+++ app/src/main/java/ru/geekbrains/calculator3/MainActivity.java
@@ -9,74 +9,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private View.OnClickListener clickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-
-            if(v.getId() == R.id.key_1){
-
-            }
-
-            if(v.getId() == R.id.key_2){
-
-            }
-
-            if(v.getId() == R.id.key_3){
-
-            }
-
-            if(v.getId() == R.id.key_4){
-
-            }
-
-            if(v.getId() == R.id.key_5){
-
-            }
-
-            if(v.getId() == R.id.key_6){
-
-            }
-
-            if(v.getId() == R.id.key_9){
-
-            }
-
-            if(v.getId() == R.id.key_0){
-
-            }
-
-            if(v.getId() == R.id.key_point){
-
-            }
-
-            if(v.getId() == R.id.key_plus){
-
-            }
-
-            if(v.getId() == R.id.key_minus){
-
-            }
-
-            if(v.getId() == R.id.key_multi){
-
-            }
-
-            if(v.getId() == R.id.key_division){
-
-            }
-
-            if(v.getId() == R.id.key_clear){
-
-            }
-
-            if(v.getId() == R.id.key_equal){
-
-            }
-
-        }
-    };
-
-
         @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -100,7 +32,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
 
+
+        }
+    };
 
 
 
--- app/src/main/java/ru/geekbrains/calculator3/operations.java
+++ app/src/main/java/ru/geekbrains/calculator3/operations.java
@@ -6,6 +6,66 @@ public class operations implements View.OnClickListener{
 
     @Override
     public void onClick(View v) {
+        if(v.getId() == R.id.key_1){
 
+        }
+
+        if(v.getId() == R.id.key_2){
+
+        }
+
+        if(v.getId() == R.id.key_3){
+
+        }
+
+        if(v.getId() == R.id.key_4){
+
+        }
+
+        if(v.getId() == R.id.key_5){
+
+        }
+
+        if(v.getId() == R.id.key_6){
+
+        }
+
+        if(v.getId() == R.id.key_9){
+
+        }
+
+        if(v.getId() == R.id.key_0){
+
+        }
+
+        if(v.getId() == R.id.key_point){
+
+        }
+
+        if(v.getId() == R.id.key_plus){
+
+        }
+
+        if(v.getId() == R.id.key_minus){
+
+        }
+
+        if(v.getId() == R.id.key_multi){
+
+        }
+
+        if(v.getId() == R.id.key_division){
+
+        }
+
+        if(v.getId() == R.id.key_clear){
+
+        }
+
+        if(v.getId() == R.id.key_equal){
+
+        }
     }
+
+
 }
