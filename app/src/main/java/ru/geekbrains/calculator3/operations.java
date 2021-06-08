c3483f0cbeccb3d27f7d667c9ca80e63b887ebc7
Nikolay <kuoluska@mail.ru>
2021-06-09 00:17:12 +0900

    начал смотреть пример препода, очень крутые идеи. я даже не представлял что можно так сделать. оч круто

--- app/src/main/java/ru/geekbrains/calculator3/operations.java
+++ app/src/main/java/ru/geekbrains/calculator3/operations.java
@@ -4,85 +4,39 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class operations extends AppCompatActivity implements View.OnClickListener{
+public class operations {
 
-    private int a = 0;
+    private int argumentOne = 0;
+    private int getArgumentTwo = 0;
+    private boolean isFirstArgument = true;
 
-    @Override
-    public void onClick(View v) {
-        if(v.getId() == R.id.key_1){
+    public operations (){
 
-            a = 1;
-        }
+    }
 
-        if(v.getId() == R.id.key_2){
-            a = 2;
-        }
+    public void keyOnePressed(){
+        computeArg(1);
+        publishArgument();
+    }
 
-        if(v.getId() == R.id.key_3){
-            a = 3;
-        }
+    private void publishArgument() {
 
-        if(v.getId() == R.id.key_4){
-            a = 4;
-        }
-
-        if(v.getId() == R.id.key_5){
-            a = 5;
-        }
-
-        if(v.getId() == R.id.key_6){
-            a = 6;
-        }
-
-        if(v.getId() == R.id.key_7){
-            a = 7;
-        }
-
-        if(v.getId() == R.id.key_8){
-            a = 8;
-        }
-
-        if(v.getId() == R.id.key_9){
-            a = 9;
-        }
-
-        if(v.getId() == R.id.key_0){
-            a = 0;
-        }
-
-        if(v.getId() == R.id.key_point){
-
-        }
-
-        if(v.getId() == R.id.key_plus){
-
-        }
-
-        if(v.getId() == R.id.key_minus){
-
-        }
-
-        if(v.getId() == R.id.key_multi){
-
-        }
-
-        if(v.getId() == R.id.key_division){
-
-        }
-
-        if(v.getId() == R.id.key_clear){
-
-        }
-
-        if(v.getId() == R.id.key_equal){
-
+        if (isFirstArgument) {
+            view.showResult(String.valueOf(argumentOne));
+        } else {
+            view.showResult(String.valueOf(argumentTwo));
         }
     }
 
-    static float calculatorLogic (int digit){
-
+    private void computeArg(int addition) {
+        if (isFirstArgument) {
+            argumentOne = argumentOne * 10 + addition;
+        } else {
+            argumentTwo = argumentTwo * 10 + addition;
+        }
 
     }
+
+    
 
 }
